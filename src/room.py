6247e957674eea8ffab179a730432b8b96675bd5
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-05-29 14:04:56 -0400

    Creates a 'rock/paper/scissors' example game

--- src/room.py
+++ src/room.py
@@ -1,4 +1,5 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 
+# Define room class before player class.
 # This should only take about 20 lines of code to define your room class.
